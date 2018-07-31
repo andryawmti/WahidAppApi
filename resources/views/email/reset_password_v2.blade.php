cf9c10ed4e22524ba8a410441858ba32581ecb18
Heri Andria Herdiana <andri@niagahoster.co.id>
2018-07-31 22:01:40 +0700

    update email =

--- resources/views/email/reset_password_v2.blade.php
+++ resources/views/email/reset_password_v2.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 
-<strong>Hello,</strong><br><br>
+<strong>Hello, {{ $user->first_name }} {{ $user->last_name }}</strong><br><br>
 
 You are receiving this email because we received a password reset request for your account.
 
