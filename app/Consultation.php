94215908af56da08e92bb51ad47952d542ffd6b1
Heri Andria Herdiana <34875303+andryawmti@users.noreply.github.com>
2018-08-19 13:50:01 +0700

    Fix/get user consultations (#11)
    * fix getUserConsultations
    
    * add getUserConsulations to model
    
    * add user id

--- app/Consultation.php
+++ app/Consultation.php
@@ -17,11 +17,11 @@ class Consultation extends Model
         return $consultations;
     }
 
-    public function getUserConsultations()
+    public function getUserConsultations($userId)
     {
         $consultations = DB::select("SELECT c.id, c.user_id, c.weight, c.sleep_time, 
         c.activity, c.calorie, c.pregnancy_age, DATE_FORMAT(c.created_at, '%d-%m-%y') as created_at
-         , DATE_FORMAT(c.updated_at, '%d-%m-%y') as updated_at FROM `consultation` as c");
+         , DATE_FORMAT(c.updated_at, '%d-%m-%y') as updated_at FROM `consultation` as c WHERE c.user_id = $userId");
         return $consultations;
     }
 }
