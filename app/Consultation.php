68bcdb0241fb8d272a28eaa3e85aee05a218c8ae
Heri Andria Herdiana <andri@niagahoster.co.id>
2018-08-19 13:46:27 +0700

    add getUserConsulations to model

--- app/Consultation.php
+++ app/Consultation.php
@@ -16,4 +16,12 @@ class Consultation extends Model
          , DATE_FORMAT(c.updated_at, '%d-%m-%y') as updated_at FROM `consultation` as c");
         return $consultations;
     }
+
+    public function getUserConsultations()
+    {
+        $consultations = DB::select("SELECT c.id, c.user_id, c.weight, c.sleep_time, 
+        c.activity, c.calorie, c.pregnancy_age, DATE_FORMAT(c.created_at, '%d-%m-%y') as created_at
+         , DATE_FORMAT(c.updated_at, '%d-%m-%y') as updated_at FROM `consultation` as c");
+        return $consultations;
+    }
 }
